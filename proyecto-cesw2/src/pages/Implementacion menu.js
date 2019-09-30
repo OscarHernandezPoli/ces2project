0ea203147800a4956238659ceb67c2ee6635a843
Miguel Gutierrez <miagutie@bancolombia.com>
2019-09-30 11:04:45 -0500

    proyecto af, v2

---
+++ proyecto-cesw2/src/pages/Implementacion menu.js
@@ -0,0 +1,21 @@
+import React from 'react';
+//menu
+import SideBar from '../components/sidebarComponent';
+import '../styles/sideMenuStyle.css';
+//form
+
+
+class Login extends React.Component {
+
+
+    render() {
+        return (
+            <div id="menu">
+                <SideBar/>
+            </div>
+
+                );
+            }
+        }
+        
+export default Login;
---
+++ proyecto-cesw2/src/pages/RegisterIncident..js
@@ -0,0 +1,17 @@
+import React from 'react';
+import txtString from '../components/txtString';
+
+class Login extends React.Component {
+
+
+    render() {
+        return (
+            <div id="formulario">
+                <txtString/>
+            </div>
+
+                );
+            }
+        }
+        
+export default Login;
