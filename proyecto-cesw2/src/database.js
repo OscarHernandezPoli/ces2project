6086fca590d7b46e2b892c3ddbc397b42fa9ab2f
sala 304 <sala304@poliacademico.edu.loc>
2019-10-11 20:09:50 -0500

    integracion con mongo atlas

--- proyecto-cesw2/src/database.js
+++ proyecto-cesw2/src/database.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb://localhost/cesw2-bd';
+const URI = 'mongodb+srv://user_admin:748596@projectcesw2-zrsle.mongodb.net/test?retryWrites=true&w=majority';
+
+//mongodb+srv://user_admin:<748596>@projectcesw2-zrsle.mongodb.net/test?retryWrites=true&w=majority
 mongoose.connect(URI)
     .then(db=>console.log('DB is connected'))
     .catch(err=>console.log(err));
